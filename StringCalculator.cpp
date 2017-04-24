6dbb80f1db82193fb40d8b852b3ad1ec1c63f7e9
Johannes Ziegenbalg <Johannes.Ziegenbalg@gmail.com>
2017-04-24 23:24:39 +0200

    First version with test.

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,5 +1,20 @@
-#include "catch.hpp"
+#include <catch.hpp>
+#include <string>
 
 class StringCalculator
-{};
+{
+    public:
+        int
+        AddString( const std::string& numbers )
+        {
+        }
 
+
+};
+
+TEST_CASE( "AddString", "StringCalculator" )
+{
+    StringCalculator calc;
+
+
+}
