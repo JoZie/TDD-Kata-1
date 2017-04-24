459806da3e8dd7c26ee9c0dee2d652a1cb8f052c
Johannes Ziegenbalg <Johannes.Ziegenbalg@gmail.com>
2017-04-24 23:49:32 +0200

    Second test case

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -7,19 +7,30 @@ class StringCalculator
         int
         AddString( const std::string& numbers )
         {
-            return 0;
-        }
+            if (numbers == "")
+                return 0;
+
+            return std::stoi( numbers );
+
+        } // AddString
 
 
 };
 
-TEST_CASE( "AddString", "StringCalculator" )
+TEST_CASE( "AddString( const std::string& numbers )", "StringCalculator" )
 {
     StringCalculator calc;
 
     SECTION( "An empty string argument should return 0" )
     {
         REQUIRE( calc.AddString( "" ) == 0 );
+    }
+
+    SECTION( "A single number string argument should return the number" )
+    {
+        REQUIRE( calc.AddString( "1" ) == 1 );
+        REQUIRE( calc.AddString( "42" ) == 42 );
+        REQUIRE( calc.AddString( "-100" ) == -100 );
     }
 
 }
